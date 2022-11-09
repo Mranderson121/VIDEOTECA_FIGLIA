7ee00bf868b0553622e425b7d9ba6e31fdeba314
roby2394 <figliaroberto@gmail.com>
2022-11-09 12:42:13 +0100

    first commit

--- src/main/java/pacchetto/dao/FilmDao.java
+++ src/main/java/pacchetto/dao/FilmDao.java
@@ -2,8 +2,35 @@ package pacchetto.dao;
 
 import javax.persistence.EntityManager;
 
+import pacchetto.dao.*;
+import pacchetto.elementi.*;
+
 public class FilmDao {
 	private static EntityManager entityManager = null;
+	
+	private void injectEntityManager() {
+		if (null == entityManager) {
+			entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+		}
+	}
+	
+	public Film getFilm(String nome) {
+		injectEntityManager();
+		Film response = entityManager.find(Film.class, nome);
+		return response;
+	}
+	
+	public Attore getAttore(String nome) {
+		injectEntityManager();
+		Attore response = entityManager.find(Attore.class, nome);
+		return response;
+	}
+	
+	public Regista getRegista(String nome) {
+		injectEntityManager();
+		Regista response = entityManager.find(Regista.class, nome);
+		return response;
+	}
 	
 
 }
